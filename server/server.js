3b22da17d823834d8a220207ca863524eae1b891
Weihao <617666549@qq.com>
2023-02-09 16:22:17 +0800

    2_9 version 1.0

--- server/server.js
+++ server/server.js
@@ -6,7 +6,8 @@ import { Configuration, OpenAIApi } from 'openai'
 dotenv.config()
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  // apiKey: process.env.OPENAI_API_KEY,
+  apiKey: "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",
 });
 
 const openai = new OpenAIApi(configuration);
@@ -16,11 +17,11 @@ app.use(cors())
 app.use(express.json())
 
 
-// app.get('/', async (req, res) => {
-//   res.status(200).send({
-//     message: 'Hello from CodeX!'
-//   })
-// })
+app.get('/', async (req, res) => {
+  res.status(200).send({
+    message: 'Hello from CodeX!'
+  })
+})
 
 app.post('/', async (req, res) => {
   try {
@@ -29,20 +30,20 @@ app.post('/', async (req, res) => {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: `${prompt}`,
-      temperature: 0, // Higher values means the model will take more risks.
-      max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+      temperature: 0.9, // Higher values means the model will take more risks.
+      max_tokens: 50, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
       top_p: 1, // alternative to sampling with temperature, called nucleus sampling
-      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+      frequency_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+      presence_penalty: 0.6, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
     });
 
     res.status(200).send({
-      bot: response.data.choices[0].text
+      bot: unescape(response.data.choices[0].text)
     });
 
   } catch (error) {
     console.error(error)
-    res.status(500).send(error || 'Something went wrong');
+    res.status(500).send(error || 'error');
   }
 })
 
